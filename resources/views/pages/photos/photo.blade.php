42949fcb26694eddda034cdf9f193dfb31874f5c
Alexander <alwong@ya.ru>
2025-05-15 23:24:29 +0400

    update

--- resources/views/pages/photos/photo.blade.php
+++ resources/views/pages/photos/photo.blade.php
@@ -6,13 +6,15 @@
             @include('pages.photos.components.header')
 
             <div class="detail__img-block detail-image-block mb-1" >
+                <div class="image-header">
+                    <h2 class="image-header__title mb-2">{{ $photo->title  }}</h2>
+                </div>
+
                 <img src="{{ Storage::url('images/photos/' . $photo->photo) }}" alt="{{ $photo->title  }}" />
 
                 @include('pages.photos.components.detail-pagination') 
             </div>
 
-            <h2 class="detail__title mb-2">{{ $photo->title  }}</h2>
-            
             <p class="detail__description mb-1">{{ $photo->description }}</p>
 
         </div>
